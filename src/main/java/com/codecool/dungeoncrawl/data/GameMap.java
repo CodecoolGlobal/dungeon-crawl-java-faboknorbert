22d8d4fbc9bdf83940ea0df8e03f72579c098e34
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2024-08-15 13:11:17 +0200

    add enemies

--- src/main/java/com/codecool/dungeoncrawl/data/GameMap.java
+++ src/main/java/com/codecool/dungeoncrawl/data/GameMap.java
@@ -1,17 +1,22 @@
 package com.codecool.dungeoncrawl.data;
 
+import com.codecool.dungeoncrawl.data.actors.Actor;
 import com.codecool.dungeoncrawl.data.actors.Player;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameMap {
     private int width;
     private int height;
     private Cell[][] cells;
-
+    private List<Actor> enemies;
     private Player player;
 
     public GameMap(int width, int height, CellType defaultCellType) {
         this.width = width;
         this.height = height;
+        this.enemies = new ArrayList<Actor>();
         cells = new Cell[width][height];
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
@@ -30,6 +35,18 @@ public class GameMap {
 
     public Player getPlayer() {
         return player;
+    }
+
+    public void addEnemy(Actor enemy){
+        enemies.add(enemy);
+    }
+
+    public void removeEnemy(Actor enemy){
+        enemies.removeIf(actor -> actor.getX() == enemy.getX() && actor.getY() == actor.getY());
+    }
+
+    public List<Actor> getEnemies() {
+        return enemies;
     }
 
     public int getWidth() {
