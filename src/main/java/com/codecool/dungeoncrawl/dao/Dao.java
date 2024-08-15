cf2b3bd9559ee19f4c40d9615de3589982bf4a8e
faboknorbert <fabok.norbert94@gmail.com>
2024-08-15 14:17:27 +0200

    Add comment to dao

--- src/main/java/com/codecool/dungeoncrawl/dao/Dao.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/Dao.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+//WIP, CURRENTLY NO USAGE
 public class Dao {
     private Connection connect(){
         String url = "jdbc:postgresql://localhost/dungeon_crawler";
