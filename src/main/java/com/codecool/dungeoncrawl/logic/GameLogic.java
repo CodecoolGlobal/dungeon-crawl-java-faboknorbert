5cb30030c99d15d99b0f7313f7cf1e751a60b61a
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2024-08-15 13:11:34 +0200

    add moveEnemy logic

--- src/main/java/com/codecool/dungeoncrawl/logic/GameLogic.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/GameLogic.java
@@ -1,12 +1,19 @@
 package com.codecool.dungeoncrawl.logic;
 
 import com.codecool.dungeoncrawl.data.Cell;
+import com.codecool.dungeoncrawl.data.CellType;
 import com.codecool.dungeoncrawl.data.GameMap;
+import com.codecool.dungeoncrawl.data.actors.Actor;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class GameLogic {
     private GameMap map;
@@ -15,6 +22,43 @@ public class GameLogic {
     public GameLogic(String mapName) {
         this.map = MapLoader.loadMap(mapName);
         this.mapNum = 1;
+    }
+
+    public void moveEnemy(KeyEvent event, GameMap map){
+        if (KeyCode.LEFT.equals(event.getCode()) || KeyCode.A.equals(event.getCode())
+                || KeyCode.RIGHT.equals(event.getCode()) || KeyCode.D.equals(event.getCode())
+                || KeyCode.UP.equals(event.getCode()) || KeyCode.W.equals(event.getCode())
+                || KeyCode.DOWN.equals(event.getCode()) || KeyCode.S.equals(event.getCode())
+        ) {
+
+            Random random = new Random();
+            for (Actor enemy : map.getEnemies()) {
+
+                List<Cell> neighbourCells = new ArrayList<>();
+                List<Cell> validCells = new ArrayList<>();
+                neighbourCells.add(enemy.getCell().getNeighbor(1, 0));
+                neighbourCells.add(enemy.getCell().getNeighbor(0, 1));
+                neighbourCells.add(enemy.getCell().getNeighbor(-1, 0));
+                neighbourCells.add(enemy.getCell().getNeighbor(0, -1));
+
+                for (Cell neighbourCell : neighbourCells) {
+                    if (neighbourCell.getType() != CellType.WALL &&
+                            neighbourCell.getType() != CellType.SAVE_CELL &&
+                            neighbourCell.getType() != CellType.LOCKED_DOOR &&
+                            neighbourCell.getActor() == null &&
+                            neighbourCell.getItem() == null) {
+                        validCells.add(neighbourCell);
+                    }
+                }
+                if(!validCells.isEmpty()) {
+                    int randomNum = random.nextInt(validCells.size());
+                    Cell neighbourCell = validCells.get(randomNum);
+                    enemy.getCell().setActor(null);
+                    enemy.setCell(neighbourCell);
+                    neighbourCell.setActor(enemy);
+                }
+            }
+        }
     }
 
     public double getMapWidth() {
