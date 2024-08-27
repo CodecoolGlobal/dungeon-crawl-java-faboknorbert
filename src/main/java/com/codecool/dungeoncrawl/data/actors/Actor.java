f95c0bb3b0224223f5f14291bc91e06a1561dcd4
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2024-08-27 11:52:12 +0200

    refactor if cell is null

--- src/main/java/com/codecool/dungeoncrawl/data/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Actor.java
@@ -19,7 +19,7 @@ public abstract class Actor implements Drawable {
 
     public void move(int dx, int dy) {
         Cell nextCell = cell.getNeighbor(dx, dy);
-        if (nextCell.canMoveTo()) {
+        if (nextCell != null && nextCell.canMoveTo()) {
             cell.setActor(null);
             nextCell.setActor(this);
             cell = nextCell;
