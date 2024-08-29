ab0c6db9898ae1285ffbdc403d42b2990405765e
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2024-08-29 09:24:23 +0200

    add IntroScene to MainStage

--- src/main/java/com/codecool/dungeoncrawl/ui/elements/MainStage.java
+++ src/main/java/com/codecool/dungeoncrawl/ui/elements/MainStage.java
@@ -1,25 +1,70 @@
 package com.codecool.dungeoncrawl.ui.elements;
 
+import com.codecool.dungeoncrawl.data.CellType;
+import com.codecool.dungeoncrawl.data.GameMap;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+import java.util.Objects;
 
 public class MainStage {
-    private Canvas canvas;
-    private Scene scene;
-    private StatusPane statusPane;
+    private final Canvas canvas;
+    private final Scene scene;
+    private final StatusPane statusPane;
+    private TextField nameField;
+    private Button startButton;
+    private VBox startScreen;
+    private GameMap gameMap;
 
-    public MainStage(Canvas canvas) {
+
+    public MainStage(Canvas canvas, GameMap map) {
         this.canvas = canvas;
-        statusPane = new StatusPane();
-        scene = setUpScene();
+        this.statusPane = new StatusPane();
+        this.scene = setUpScene();
+        this.gameMap = map;
     }
 
     private Scene setUpScene() {
         BorderPane borderPane = statusPane.build();
+        borderPane.setStyle("-fx-background-color: " + "#482c3c" + ";");
         borderPane.setCenter(canvas);
-        Scene scene = new Scene(borderPane);
+        setupStartScreen(borderPane);
+        Scene scene = new Scene(startScreen);
         return scene;
+    }
+
+    private void setupStartScreen(BorderPane gamePane) {
+        nameField = new TextField();
+        nameField.setPromptText("Enter your name");
+        startButton = new Button("Start Game");
+        VBox vbox = new VBox();
+        vbox.setAlignment(Pos.BASELINE_CENTER);
+        startScreen = vbox;
+
+        startScreen.getChildren().addAll(new Label("Welcome to Dungeon Crawl!"), nameField, startButton);
+
+        startButton.setOnAction(event -> {
+            if (!nameField.getText().trim().isEmpty()) {
+                setNameLabelText(nameField.getText());
+                if(Objects.equals(nameField.getText(), "Adem")){
+                    gameMap.getPlayer().setName(nameField.getText());
+
+                }
+                switchToGameView(gamePane);
+            } else {
+                System.out.println("Name cannot be empty!");
+            }
+        });
+    }
+
+    private void switchToGameView(BorderPane gamePane) {
+        scene.setRoot(gamePane);
     }
 
     public Scene getScene() {
@@ -28,6 +73,10 @@ public class MainStage {
 
     public void setHealthLabelText(String text) {
         this.statusPane.setHealthValue(text);
+    }
+
+    public void setNameLabelText(String name) {
+        this.statusPane.setNameValue(name);
     }
 
     public void setDamageLabelText(String text) {
