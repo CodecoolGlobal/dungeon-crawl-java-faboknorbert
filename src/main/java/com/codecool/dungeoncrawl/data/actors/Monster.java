34486af70f9196ac31172af9fe6b93aaab9abaa0
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2024-08-29 09:46:19 +0200

    add enemy move method

---
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Monster.java
@@ -0,0 +1,5 @@
+package com.codecool.dungeoncrawl.data.actors;
+
+public interface Monster {
+    void move();
+}
--- src/main/java/com/codecool/dungeoncrawl/data/actors/Tarantula.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Tarantula.java
@@ -2,13 +2,38 @@ package com.codecool.dungeoncrawl.data.actors;
 
 import com.codecool.dungeoncrawl.data.Cell;
 
-public class Tarantula  extends Actor {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Tarantula  extends Actor implements Monster{
     public Tarantula(Cell cell, int health, int strength) {
         super(cell, health, strength);
     }
+    Random random = new Random();
 
     @Override
     public String getTileName() {
         return "tarantula";
+    }
+
+
+    @Override
+    public void move(){
+        List<Cell> neighbourCells = new ArrayList<>();
+
+        for (Cell neighbourCell : this.getCell().getNeighbors()) {
+            if (neighbourCell.canMoveToCellType(this)) {
+                neighbourCells.add(neighbourCell);
+            }
+        }
+
+        if(!neighbourCells.isEmpty()) {
+            int randomNum = random.nextInt(neighbourCells.size());
+            Cell neighbourCell = neighbourCells.get(randomNum);
+            this.getCell().setActor(null);
+            this.setCell(neighbourCell);
+            neighbourCell.setActor(this);
+        }
     }
 }
